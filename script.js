a3fa25da01440210b5475aec40ef0530944a7506
Viktoryia Danko <viktoryia.danko@LeverX.local>
2022-08-23 17:30:33 +0300

    add logics

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 document.querySelector("#add-to-do").onclick = function () {
   if (document.querySelector("input").value.length == 0) {
     alert("Please enter a task");
+  } else {
+    document.querySelector(".list").innerHTML += ` 
+    <div class="list-item">
+    <li>  ${document.querySelector("input").value}</li>
+    <button class="bin"></button>
+  </div>
+                  
+`;
+
+    let taskToDo = document.querySelectorAll(".bin");
+    for (let i = 0; i < taskToDo.length; i++) {
+      taskToDo[i].onclick = function () {
+        this.parentNode.remove();
+      };
+    }
+
+    let listOfTasks = document.querySelectorAll(".list-item");
+    for (let i = 0; i < listOfTasks.length; i++) {
+      listOfTasks[i].onclick = function () {
+        this.classList.toggle("completed");
+      };
+    }
+
+    document.querySelector("input").value = "";
   }
 };
